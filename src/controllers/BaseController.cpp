fad9dd4e1311dce727b7d05daebb21a986ef68cd
jazdzyk <kikiri123@gmail.com>
2019-08-16 00:57:58 +0200

    create Localizable namespace create BaseController class skeleton

--- src/controllers/BaseController.cpp
+++ src/controllers/BaseController.cpp
@@ -8,6 +8,42 @@ BaseController::BaseController(QWidget *parent) {
 
 }
 
+BaseController::BaseController(SimulationSettings &simulationSettings, QWidget *parent) {
+
+}
+
 BaseController::~BaseController() {
 
+}
+
+void BaseController::setSimulationSettings(SimulationSettings &simulationSettings) {
+
+}
+
+void BaseController::addToParentLayout(QLayout *layout, int stretch) {
+
+}
+
+void BaseController::addToParentLayout(QWidget *widget, int stretch) {
+
+}
+
+void BaseController::addWidgetsToLayout(QWidget &widgets, QLayout *layout) {
+
+}
+
+void BaseController::setUpUi() {
+
+}
+
+void BaseController::createQuitButton() {
+
+}
+
+void BaseController::createReturnButton() {
+
+}
+
+QPushButton *BaseController::createButton(const QString &iconUrl) {
+    return nullptr;
 }
--- src/controllers/BaseController.h
+++ src/controllers/BaseController.h
@@ -7,13 +7,42 @@
 
 
 #include <QtWidgets/QMainWindow>
+#include <src/utils/SimulationSettings.h>
+#include <QtWidgets/QPushButton>
+#include <src/utils/Localizable.h>
+#include <QtWidgets/QHBoxLayout>
 
 class BaseController : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit BaseController(QWidget *parent = nullptr);
+    explicit BaseController(SimulationSettings &simulationSettings, QWidget *parent = nullptr);
     ~BaseController();
+
+    void setSimulationSettings(SimulationSettings &simulationSettings);
+
+protected:
+    using QWidgets = const std::vector<QWidget*>;
+
+    QPushButton *quitButton;
+    QPushButton *returnButton;
+
+    Localizable::Locale locale = Localizable::PL;
+    SimulationSettings simulationSettings;
+
+    void addToParentLayout(QLayout *layout, int stretch = 0);
+    void addToParentLayout(QWidget *widget, int stretch = 0);
+    void addWidgetsToLayout(QWidget &widgets, QLayout *layout);
+
+private:
+    QHBoxLayout *parentLayout;
+
+    void setUpUi();
+    void createQuitButton();
+    void createReturnButton();
+
+    QPushButton* createButton(const QString &iconUrl);
 };
 
 
