7c944905da97cf83bb4ee8327222ff84c2e96574
jazdzyk <kikiri123@gmail.com>
2019-08-29 23:01:57 +0200

    implement SimulationStateView functionalities

---
+++ src/views/ArrowsView.h
@@ -0,0 +1,14 @@
+//
+// Created by Kuba Jazdzyk on 8/29/19.
+//
+
+#ifndef MICROMOUSE_ARROWSVIEW_H
+#define MICROMOUSE_ARROWSVIEW_H
+
+
+class ArrowsView {
+
+};
+
+
+#endif //MICROMOUSE_ARROWSVIEW_H
--- src/views/SimulationStateView.h
+++ src/views/SimulationStateView.h
@@ -6,8 +6,42 @@
 #define MICROMOUSE_SIMULATIONSTATEVIEW_H
 
 
-class SimulationStateView {
+#include <QtWidgets/QFrame>
+#include <src/utils/Enums.h>
+#include <src/utils/Localizable.h>
+#include <QtWidgets/QLabel>
+#include <src/utils/Logging.h>
+#include <QtWidgets/QHBoxLayout>
+#include "ArrowsView.h"
+#include <src/utils/utils.h>
 
+class SimulationStateView : public QFrame {
+public:
+    SimulationStateView();
+    ~SimulationStateView() override;
+
+    void setName(const QString& name) const;
+
+    void setTimeValue(double time);
+    void setDistanceValue(int distance);
+    void setSpeedValue(double speed);
+
+    void activateArrow(Direction direction) const;
+    void deactivateAllArrows() const;
+
+private:
+    Localizable::Locale locale = Localizable::pl;
+
+    QLabel *nameLabel;
+
+    QLabel *timeValueLabel;
+    QLabel *distanceValueLabel;
+    QLabel *speedValueLabel;
+
+    ArrowsView *arrowsView;
+
+    void setUpUi();
+    QHBoxLayout* prepareValuesLayout();
 };
 
 
