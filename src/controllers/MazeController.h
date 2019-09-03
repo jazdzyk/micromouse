65d34cdaf454d7324859b87ced528986cd206472
jazdzyk <kikiri123@gmail.com>
2019-09-03 20:53:52 +0200

    document MazeController

--- src/controllers/MazeController.h
+++ src/controllers/MazeController.h
@@ -16,14 +16,35 @@
 #include <utility>
 #include <QFile>
 
+/**
+ * The MazeController class inherits from BaseController and is a controller class
+ * for a window related to own maze's creation.
+ *
+ * It enables:
+ *  - 'building' a maze by a user,
+ *  - loading a maze from a binary file,
+ *  - saving a maze to the binary file.
+ */
 class MazeController : public BaseController {
     Q_OBJECT
 
 public:
+    /**
+     * MazeController class constructor.
+     *
+     * @param simulationSettings a set of settings used to perform Micromouse simulation
+     * @param mazeControllerDelegate an optional delegate for MazeController
+     * @param returnDelegate an optional delegate due to the purpose of returning to the previous controller
+     * @param parent a parent class to the MazeController
+     */
     explicit MazeController(SimulationSettings &simulationSettings,
                             std::optional<MazeControllerDelegate *> mazeControllerDelegate,
                             std::optional<ReturnToPreviousControllerDelegate *> returnDelegate,
                             QWidget *parent = nullptr);
+
+    /**
+     * MazeController class destructor.
+     */
     ~MazeController() override;
 
 private:
@@ -39,13 +60,43 @@ private:
     MazeView *mazeView;
     MazeHolder *mazeHolder;
 
+    /**
+     * A private method which enables a configuration of controller's UI.
+     */
     void setUpUi();
-    QVBoxLayout* prepareButtonsLayout();
+
+    /**
+     * A private method which prepares layout with buttons for displaying in a UI.
+     *
+     * @return a layout with buttons
+     */
+    QVBoxLayout *prepareButtonsLayout();
+
+    /**
+     * A private method which assigns MazeView into its placeholder.
+     *
+     * @param mazeView a MazeView object to be assigned into placeholder
+     */
     void setMazeHolder(MazeView *mazeView);
 
+    /**
+     * A private method which is a callback for AcceptButton.
+     */
     void onAcceptButtonClicked();
+
+    /**
+     * A private method which is a callback for SaveButton.
+     */
     void onSaveButtonClicked();
+
+    /**
+     * A private method which is a callback for LoadButton.
+     */
     void onLoadButtonClicked();
+
+    /**
+     * A private method which is a callback for ReturnButton.
+     */
     void onReturnButtonClicked();
 };
 
