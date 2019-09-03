7d4a5584b36914f527c63de5fafefba4153ec766
jazdzyk <kikiri123@gmail.com>
2019-09-03 21:45:42 +0200

    document CheckButtonGroupBox

--- src/elements/CheckButtonGroupBox.h
+++ src/elements/CheckButtonGroupBox.h
@@ -13,22 +13,54 @@
 #include <src/elements/RoundedPushButton.h>
 #include <QtWidgets/QVBoxLayout>
 
+/**
+ * The CheckButtonGroupBox class inherits from QGroupBox and extends its functionalities.
+ * It contains pairs of a checkbox and a button. When a checkbox is checked, the button is enabled.
+ */
 class CheckButtonGroupBox : public QGroupBox {
 public:
     using ActionCallback = std::optional<std::function<void(int)>>;
     using ButtonTitles = const std::vector<QString>;
 
+    /**
+     * CheckButtonGroupBox class constructor.
+     *
+     * @param title a title for a box
+     * @param buttonTitles a vector of titles for buttons
+     * @param onButtonsClicked a callback for buttons
+     */
     explicit CheckButtonGroupBox(const QString &title, ButtonTitles &buttonTitles, ActionCallback onButtonsClicked);
-    ~CheckButtonGroupBox();
 
-    bool isChosen(const int id) const;
+    /**
+     * CheckButtonGroupBox class destructor.
+     */
+    ~CheckButtonGroupBox() override;
+
+    /**
+     * A public method which informs whether a pair with a given id is currently chosen.
+     *
+     * @param id an id of a pair checkbox-button
+     * @return an appropriate boolean flag
+     */
+    [[nodiscard]] bool isChosen(int id) const;
 
 private:
     using ButtonGroup = std::pair<int, std::pair<QCheckBox*, RoundedPushButton*>>;
 
     std::vector<ButtonGroup> buttonGroups;
 
+    /**
+     * A private method which enables a configuration of box's UI.
+     *
+     * @param buttonTitles a vector of titles for buttons
+     */
     void setUpUi(ButtonTitles &buttonTitles);
+
+    /**
+     * A private method which enables setting the callback action for buttons.
+     *
+     * @param onButtonsClicked a callback for buttons
+     */
     void setButtonsAction(ActionCallback onButtonsClicked);
 };
 
