0ee72e74e0c641893a70ed5a4c5bd67352581711
jazdzyk <kikiri123@gmail.com>
2019-09-03 22:15:20 +0200

    document RoundedPushButton

--- src/elements/RoundedPushButton.h
+++ src/elements/RoundedPushButton.h
@@ -11,15 +11,34 @@
 #include <optional>
 #include "src/utils/Logging.h"
 
+/**
+ * The RoundedPushButton class inherits from QPushButton and customizes its style.
+ */
 class RoundedPushButton : public QPushButton {
 public:
     using ActionCallback = std::optional<std::function<void()>>;
 
+    /**
+     * RoundedPushButton class constructor.
+     *
+     * @param title a title for a button
+     * @param padding a padding size value
+     */
     explicit RoundedPushButton(const QString &title = "", int padding = 0);
 
+    /**
+     * A public method which enables setting the callback action for a button.
+     *
+     * @param onButtonClicked a callback for a button
+     */
     void setAction(ActionCallback onButtonClicked);
 
 private:
+    /**
+     * A private method which customizes button's style.
+     *
+     * @param padding a padding size value
+     */
     void setUpStyle(int padding);
 };
 
