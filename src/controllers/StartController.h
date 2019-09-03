0d7bbb0739f8671b5112d25764557cbdcbd9aa96
jazdzyk <kikiri123@gmail.com>
2019-09-03 21:31:56 +0200

    document StartController

--- src/controllers/StartController.h
+++ src/controllers/StartController.h
@@ -16,11 +16,30 @@
 #include "SimulationController.h"
 #include "MazeController.h"
 
+/**
+ * The StartController class inherits from BaseController
+ * and implements MazeControllerDelegate and ReturnToPreviousControllerDelegate.
+ * It is a controller class for a window related to the start view.
+ *
+ * It enables:
+ *  - a configutation of the simulation to be performed,
+ *  - moving to a window related to the simulation,
+ *  - moving to a window related to user's own maze creation.
+ */
 class StartController : public BaseController, public MazeControllerDelegate, public ReturnToPreviousControllerDelegate {
     Q_OBJECT
 
 public:
+    /**
+     * StartController class constructor.
+     *
+     * @param parent a parent class to the StartController
+     */
     explicit StartController(QWidget *parent = nullptr);
+
+    /**
+     * StartController class destructor.
+     */
     ~StartController() override;
 
 private:
@@ -30,24 +49,72 @@ private:
     DropDownGroupBox *algorithmModeBox;
     CheckButtonGroupBox *ownMazeBox;
 
+    /**
+     * A private method which enables a configuration of controller's UI.
+     */
     void setUpUi();
 
+    /**
+     * A private template function which enables moving to the next controller.
+     *
+     * @tparam Type a type of the next controller
+     * @param newController an instance of the next controller
+     */
     template<typename Type>
     void moveToNextController(Type *newController) {
         newController->showFullScreen();
         hide();
     }
 
+    /**
+     * A private method which is a callback for StartSimulationButton.
+     */
     void onStartSimulationButtonClicked();
+
+    /**
+     * A private method which is a callback for SimulationMode box with radio buttons.
+     *
+     * @param id an id of a chosen radio button
+     */
     void onSimulationModeChanged(int id);
+
+    /**
+     * A private method which is a callback for MazeSize box with radio buttons.
+     *
+     * @param id an id of a chosen radio button
+     */
     void onMazeSizeChanged(int id);
+
+    /**
+     * A private method which is a callback for AlgorithmMode box with a drop-down list.
+     *
+     * @param dropDownId an id of a used drop-down list
+     * @param optionId an id of the chosen option in a list
+     */
     void onAlgorithmModeChanged(int dropDownId, int optionId);
+
+    /**
+     * A private method which is a callback for OwnMaze box with a checkbox and a button.
+     *
+     * @param id an id of a checkbox clicked
+     */
     void onCreateOwnMazeButtonClicked(int id);
 
     // MazeControllerDelegate methods
+    /**
+     * A private method which delegates work when MazeController did accept a maze.
+     *
+     * @param controller an instance of the MazeController
+     * @param simulationSettings a set of settings used to perform Micromouse simulation
+     */
     void mazeControllerDidAcceptMaze(BaseController *controller, SimulationSettings &simulationSettings) override;
 
     // ReturnToPreviousControllerDelegate methods
+    /**
+     * A private method which delegates work when a controller will return.
+     *
+     * @param controller an instance of the returning controller
+     */
     void controllerWillReturn(BaseController *controller) override;
 };
 
